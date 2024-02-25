8691a746d24bc389ecb5fbc3e103869adc8a85bc
mehrzad <merzaad@gmail.com>
2024-02-25 09:12:47 +0330

    optimize

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import { useState, useEffect } from "react";
 
 const useMatchMedia = (query) => {
   const [match, setMatch] = useState(false);
-  const matchMedia = window.matchMedia(query);
   useEffect(() => {
+    const matchMedia = window.matchMedia(query);
     matchMedia.onchange = () => {
       setMatch(matchMedia.matches);
     };
     return () => {
       matchMedia.onchange = null;
     };
-  }, []);
+  }, [query]);
 
   return match;
 };
